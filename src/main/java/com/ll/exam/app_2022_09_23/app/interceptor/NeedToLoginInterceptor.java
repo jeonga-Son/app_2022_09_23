6f0a479fd88a2b80f9b9076041216dcb6274a334
jeonga <jjeong0106@gmail.com>
2022-09-28 13:53:14 +0900

    로그인

--- src/main/java/com/ll/exam/app_2022_09_23/app/interceptor/NeedToLoginInterceptor.java
+++ src/main/java/com/ll/exam/app_2022_09_23/app/interceptor/NeedToLoginInterceptor.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Component
 @Slf4j
@@ -14,9 +15,13 @@ public class NeedToLoginInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.debug("NeedToLoginInterceptor::preHandle 실행됨");
 
-        boolean isLogined = false; // 임시
+        HttpSession session = request.getSession();
 
-        if (isLogined == false) {
+        Long loginedMemberId = (Long) session.getAttribute("loginedMemberId");
+
+        boolean isLogined = loginedMemberId != null; // 임시
+
+       if (isLogined == false) {
             response.setCharacterEncoding("UTF-8");
             response.setContentType("text/html; charset=UTF-8");
             response.getWriter().append("로그인 후 이용바랍니다.");
